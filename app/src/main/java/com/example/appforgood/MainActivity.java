84149acfcec0b148326ca703cdaba445fa9eb70e
dwhiting14 <dylanwhiting14@gmail.com>
2024-03-25 11:57:01 -0400

    i hope this works

--- app/src/main/java/com/example/appforgood/MainActivity.java
+++ app/src/main/java/com/example/appforgood/MainActivity.java
@@ -1,8 +1,11 @@
         package com.example.appforgood;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
@@ -16,7 +19,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.appforgood.databinding.ActivityMainBinding;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.Random;
+
+        public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityMainBinding binding;
@@ -48,7 +54,47 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
+
+
+
+        ArrayList<ArrayList> categories = new ArrayList<>();
+        ArrayList<String> nature = new ArrayList<>();
+        ArrayList<String> music = new ArrayList<>();
+        ArrayList<String> sports = new ArrayList<>();
+
+        categories.add(nature);
+        categories.add(music);
+        categories.add(nature);
+        nature.add("Go for a hike"); nature.add("Have a picnic in the park"); nature.add("Go birdwatching");
+        music.add("Go to a concert"); music.add("Listen to music on spotify"); music.add("Sing karaoke with friends");
+        sports.add("Play basketball"); sports.add("Play ping pong"); sports.add("Play 2k");
+
+
+        //Find button
+        Button btn = findViewById(R.id.clickforidea);
+        //Set button behavior
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("firstapp", "CLICKED - GIVE IDEA");
+
+                Random r = new Random();
+                int catIndex = r.nextInt(categories.size());
+                ArrayList<String> selectedCat = new ArrayList<>();
+                selectedCat = categories.get(catIndex);
+
+                int actIndex = r.nextInt(selectedCat.size());
+                String activity = selectedCat.get(actIndex);
+
+                Toast.makeText(MainActivity.this, activity, Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
+
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
